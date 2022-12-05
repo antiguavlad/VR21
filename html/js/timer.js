e697dad1e5afec51e170a9ded882dbcd09cdfab9
Vladimir Antigua <vladimirantigua@gmail.com>
2022-12-05 09:07:27 -0800

    timer.js

---
+++ html/js/timer.js
@@ -0,0 +1,5 @@
+// Set time out: 
+
+setTimeout(() => {
+    console.log("Print me in 3 seconds "); // all the code inside here will execute in the set time in this case is in 3 seconds
+}, 3000); // 3 seconds in miliseconds
