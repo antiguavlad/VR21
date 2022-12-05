87db240777902fbb0bb4a3d742e2bdfd527d5067
Vladimir Antigua <vladimirantigua@gmail.com>
2022-12-05 09:24:09 -0800

    timer.js

--- html/js/timer.js
+++ html/js/timer.js
@@ -1,5 +1,27 @@
 // Set time out: 
 
-setTimeout(() => {
-    console.log("Print me in 3 seconds "); // all the code inside here will execute in the set time in this case is in 3 seconds
-}, 3000); // 3 seconds in miliseconds
+// setTimeout(() => {
+//     console.log("Print me in 3 seconds "); // all the code inside here will execute in the set time in this case is in 3 seconds
+// }, 3000); // 3 seconds in miliseconds
+
+let timeId;
+let counter = 0;
+
+function incrementTime() {
+    counter++;
+    document.querySelector("#count").innerText = counter;
+
+}
+
+function start() {
+
+    if (!timeId) {
+        timeId = setInterval(incrementTime, 1000);
+    }
+}
+
+function stop() {
+    clearInterval(timeId);
+    timeId = null;
+
+}
