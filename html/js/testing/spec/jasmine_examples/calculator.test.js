6dd2b2aae26d0ab1dba2e5b6cccc2052cf54e373
Vladimir Antigua <vladimirantigua@gmail.com>
2022-12-08 15:45:48 -0800

    adding the calculator

---
+++ html/js/testing/spec/jasmine_examples/calculator.test.js
@@ -0,0 +1,8 @@
+const Song = require('../../lib/jasmine_examples/calcularor');
+it('should calculate the monthly rate correctly', function () {
+    expect(calcMonthlyPayment(10000, 8, 5.8)).toEqual(130.44263011109317);
+});
+
+it("should handle terribly high interest rates", function () {
+    expect(calcMonthlyPayment(1000, 40, 99)).toEqual(82.5);
+});
