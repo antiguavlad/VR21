99598cd482050235ca343e551a0da8ba8395f207
Vladimir Antigua <vladimirantigua@gmail.com>
2022-12-09 14:01:24 -0800

    api.html api.js BootStrap API NBA Players

--- html/js/api.js
+++ html/js/api.js
@@ -1,24 +1,38 @@
+    //THIS IS THE SUDO CODE TO DO THE API PROCESS ABOVE
+    //1. get api key
+    //2. capture search term
+    
+    //3. make a request to unsplash api by using fetch which use HTTP request 
+    //4. capture the API data
+    //5. loop over the data and 
+    //6. append to the DOM
+
+    //const client_id = ""; // for the API key
 // I am using my keys from the unsplash: 
 
 const client_id = "bPWVNq3Jw4IKnzObHBgDiC8KoAXPCl7oXg1ldS8TxgU";
+//capture the search term by using const searchStr = "";
 const searchStr = "NBA Players";
 
-
+//fetch takes takes a resources which is a url perform the git request
+//accesskey client_id=${client_id}
+//query: query=${searchStr}
 fetch(
     `https://api.unsplash.com/search/photos?query=${searchStr}&per_page=20&client_id=${client_id}`
 )
 
     //we get the promise by using .then indicate returning a promise
-    .then((response) => response.json())
+    //receive response   return response
+    .then((response) => response.json()) //return a response in json
     // second .then is to do the work
     .then((data) => {
-        // container
+        // to capture the data container to append the images 
         const container = document.querySelector(".container");
-        // looping over the container: 
+        // looping over the container wit hthe data captured in the data variable  
         for (let i = 0; i < data.results.length - 1; i++) {
             const img = document.createElement("img");
             img.setAttribute("src", data.results[i].urls.thumb);
-
+            // appending the images to the container in the html
             container.append(img);
         }
     });
@@ -27,4 +41,17 @@ fetch(
     //endpoint
     // query  = ?
     //concatenated by =  &
+    // amount of photos to search = per_page=20
+    //client id = client_id=${client_id}
     //`https://api.unsplash.com/search/photos?query=${searchStr}&per_page=20&client_id=${client_id}`
+
+    //THIS IS THE SUDO CODE TO DO THE API PROCESS ABOVE
+    //1. get api key
+    //2. capture search term
+    
+    //3. make a request to unsplash api by using fetch which use HTTP request 
+    //4. capture the API data
+    //5. loop over the data and 
+    //6. append to the DOM
+
+
